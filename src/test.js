63deaa57f526d0f0c2d6ef3a3259c82bd7e3acb4
ling <l13789060033@163.com>
2021-03-24 16:42:59 +0800

    优化

--- src/test.js
+++ src/test.js
@@ -1,56 +1,67 @@
 import string from './css.js'
 
-let n = 1
-demo1.innerText = string.substr(0, n)
-demo2.innerHTML = string.substr(0, n)
+const demo1 = document.querySelector('#demo1')
+const demo2 = document.querySelector('#demo2')
 
-let time = 100
-
-
-const run = () => {
-    n += 1
-    if (n > string.length) {
-        window.clearInterval(id)
-        return
+const player = {
+    id: undefined,
+    time: 100,
+    ui: {
+        demo1: document.querySelector('#demo1'),
+        demo2: document.querySelector('#demo2')
+    },
+    events = {
+        '#btnPause': 'pause',
+        '#btnPlay': 'pause',
+        '#btnSlow': 'slow',
+        '#btnNormal': 'normal',
+        '#btnFast': 'fast',
+    },
+    n: 1,
+    init: () => {
+        player.ui.demo1.innerText = string.substr(0, player.n)
+        player.ui.demo2.innerHTML = string.substr(0, player.n)
+        player.play()
+        player.bindEvents()
+    },
+    bindEvents: () => {
+        for (let key in player.events)
+            if (player.events.hasOwnProperty(key)) {
+                const value = player.events[key]
+                document.querySelector(key).onclick = player[value]
+            }
+    },
+    run: () => {
+        player.n += 1
+        if (player.n > string.length) {
+            window.clearInterval(id)
+            return
+        }
+        console.log(n + ':' + string.substr(0, player.n))
+        demo1.innerText = string.substr(0, player.n)
+        demo2.innerHTML = string.substr(0, player.n)
+        demo1.scrollTop = demo.scrollHeight
+    },
+    play: () => {
+        player.id = setInterval(player.run, player.time)
+    },
+    pause: () => {
+        window.clearInterval(player.id)
+    },
+    slow: () => {
+        player.pause()
+        player.time = 300
+        player.play()
+    },
+    normal: () => {
+        player.pause()
+        player.time = 100
+        player.play()
+    },
+    fast: () => {
+        player.pause()
+        player.time = 0
+        player.play()
     }
-
-
-    console.log(n + ':' + string.substr(0, n))
-    demo1.innerText = string.substr(0, n)
-    demo2.innerHTML = string.substr(0, n)
-    demo1.scrollTop = demo.scrollHeight
 }
-
-const play = () => {
-    return setInterval(run, time)
-}
-
-const pause = () => {
-    window.clearInterval(id)
-}
-
-let id = play()
-
-btnPause.onclick = () => {
-    pause()
-}
-
-btnPlay.onclick = () => {
-    id = play()
-}
-
-btnSlow.onclick = () => {
-    pause()
-    time = 300
-    id = play()
-}
-btnNormal.onclick = () => {
-    pause()
-    time = 100
-    id = play()
-}
-btnFast.onclick = () => {
-    pause()
-    time = 0
-    id = play()
-}
+player.init()
